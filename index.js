5b209034985e719cc1aa6ed35eb968646f0e80f1
Abdul Munam <abdulmunam791@gmail.com>
2025-04-06 04:28:40 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let age = 45;
+if(age>18){
+    console.log("so you can drive")
+}
+else{
+    console.log("you can not drive")
+}
---
+++ script.js
@@ -0,0 +1,10 @@
+var a = 5;
+var b = 9;
+console.log(a + b)
+let obj = {
+    name: "harry",
+    "job role": 5600,
+}
+console.log(obj)
+obj.salary = "100 crores",
+console.log(obj)
