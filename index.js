4492c16466d08af170d34947b66d13da7e9261ad
Abdul Munam <abdulmunam791@gmail.com>
2025-04-06 04:29:44 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-let age = 45;
-if(age>18){
-    console.log("so you can drive")
+
+function bla (name) {
+    console.log("hey " + name + "harry whats your name")
+console.log("hey " + name+ "harry whats your age")
+console.log("hey " + name+ " harry your course is good")
+console.log("hey " + name+ " harry thats all")
+
 }
-else{
-    console.log("you can not drive")
+bla("Munam")
+bla("you you")
+function sum(a , b, c = 16) {
+    // console.log(a * b)
+    return a + b + c
 }
+ result = sum(5, 29)
+
+console.log("the sum of numbers are:",result)
+
+let func1 = (x)=>{
+    console.log("iam a an arrow bla bla bla", x)
+}
+func1(34);
