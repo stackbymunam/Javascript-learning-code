a857ad9ac3169681db9bebcdad2753d57a5e1800
Abdul Munam <abdulmunam791@gmail.com>
2025-04-07 03:10:29 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// let boxes = document.getElementsByClassName("box")
-// console.log(boxes)
-
-
-// boxes[2].style.backgroundColor = "red"
-
-// document.getElementById("redbox").style.backgroundColor = "red"
-// document.querySelector(".box").style.backgroundColor = "red"
-
-document.querySelectorAll(".box").forEach(e =>{
-    e.style.backgroundColor = "red"
+let button = document.getElementById("btn")
+button.addEventListener("dblclick", ()=>{
+    document.querySelector(".box").innerHTML = " <b> yayy i was clicked i knoew it </B>"
 })
 
-
+button.addEventListener("contextmenu", ()=>{
+    alert("dont right click please it broser will turn into dark wed")
+    document.querySelector(".box").style.backgroundColor = "blue"
+})
+document.addEventListener("keydown", (e)=>{
+    console.log(e)
+})
