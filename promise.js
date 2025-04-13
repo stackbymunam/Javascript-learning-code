837006afb53525560cad4d71d7b1f86b3fce31f6
Abdul Munam <abdulmunam791@gmail.com>
2025-04-14 03:39:38 +0500

    Add files via upload

--- promise.js
+++ promise.js
@@ -1,15 +1,37 @@
-let prom = new Promise((resolve, reject)) => {
-    let a = Math.random
-    if (a < 0.5) {
-        reject("no we are not suppoting this ")
-    }
-    else {
-        setTimeout(() => {
-            console.log("yes iam done");
-            resolve("harry")
-        }, 2000);
-    }
-    prom1.then(a) =>{
-        console.log(a)
-    }
+let prom1 = new Promise((resolve, reject) => {
+let a = Math.random();
+if(a>0.5){
+    reject("no random number are supporting you ")
 }
+else{
+    setTimeout(() => {
+        console.log("yeah iam inn bal bla bla")
+        resolve("harry")
+    }, 3000);
+}
+
+
+})
+
+let prom2 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if(a>0.5){
+        reject("no random number are supporting you ")
+    }
+    else{
+        setTimeout(() => {
+            console.log("yeah iam inn bal bla bla")
+            resolve("harry")
+        }, 3000);
+    }
+    
+    
+    })
+
+
+let p3 = Promise.race([prom1, prom2])
+p3.then((a)=>{
+    console.log(a)
+}).catch(err=>{
+    console.log(err)
+})
--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const callback = (arg) =>{
+console.log("munam is a hackder");
+console.log("he is a hecker")
+
+setTimeout(() => {
+    console.log("iam inside timeout bla bla bla")
+}, 2000);
+
+console.log("the end : thats it")
+
+
+
+const callback = (arg)=>{
     console.log(arg)
 }
-
-
-const loadscript = (src, callback) => {
-    let sc = document.createElement("script");
-    sc.src = src;
-    sc.onload = callback("Munam")
-
+const loadscritp = (src, callback)=>{
+    let sc = document.createElement("script")
+    sc.src = src
+    sc.onload =  callback("harry");
+    document.head.append(sc)
 }
+
+loadscritp("",)
+
