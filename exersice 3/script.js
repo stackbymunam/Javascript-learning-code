d52f74975153a441b81ae7f3907fcf55f8ba8482
Abdul Munam <abdulmunam791@gmail.com>
2025-04-24 02:34:04 +0500

    Add files via upload

---
+++ exersice 3/script.js
@@ -0,0 +1,19 @@
+
+    let coldjokes = jokes = [
+        "Why don’t scientists trust atoms? Because they make up everything.",
+        "I told my computer I needed a break, and it said 'No problem, I’ll go to sleep.'",
+        "Why do programmers prefer dark mode? Because light attracts bugs.",
+        "Parallel lines have so much in common… it’s a shame they’ll never meet.",
+        "Why was the math book sad? It had too many problems.",
+        "I would tell you a UDP joke, but you might not get it.",
+        "I'm reading a book about anti-gravity. It's impossible to put down!",
+        "I asked the dog, 'What's two minus two?' He said nothing.",
+        "Why don't skeletons fight each other? They don't have the guts.",
+        "I told my wife she was drawing her eyebrows too high. She looked surprised."
+    ]
+
+    let random = Math.floor(Math.random() * 10)
+     let jok =document.getElementById("jokes").innerHTML = jokes[random]
+
+
+    
