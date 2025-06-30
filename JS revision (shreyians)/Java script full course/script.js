57538954c29d076bbcede12085ad5ec56918f348
Abdul Munam <abdulmunam791@gmail.com>
2025-07-01 03:12:45 +0500

    Add files via upload

---
+++ JS revision (shreyians)/Java script full course/script.js
@@ -0,0 +1,68 @@
+// var let const//
+
+// declaritions and initilization//
+
+// var a; // declare karna//
+// var a = 12; // declare and intilaize karna//
+
+    // next topic//
+
+    // reassigment and redeclartion
+
+
+    var a = 12;
+    a = 32;
+
+    // redeclarition is allowd in var but in js is not allowed
+
+    let b = 34;
+    b = 42;
+
+
+    // next topic//
+
+    // Temporal dead zone
+    let v = 12 + 4
+
+
+
+/// this space is called a tdz
+
+
+
+console.log(v);
+
+
+// next topic
+
+
+// hoisting impact per type
+
+// hoisting -> ek variable ko jab js main banate hai to wo variable do hisso main toot jata hai and uska decalre part uper chala  jata hai and uska intialize part neeche reh jata hai
+
+var a = undefined;
+
+
+a = 12
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
