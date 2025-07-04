62cc77e8786afa9f4302d0b477722ee239bb5611
Abdul Munam <abdulmunam791@gmail.com>
2025-07-05 04:05:30 +0500

    Add files via upload

---
+++ JS revision (shreyians)/Java script full course/loops/script.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+// question 1
+
+for(let i = 1 ; i<11; i++){
+    console.log(i)
+};
+
+
+// question 2
+
+ let i = 10;
+ while(i > 0){
+    console.log(i)
+    i--
+ }
+
+
+//question 3
+
+
+
