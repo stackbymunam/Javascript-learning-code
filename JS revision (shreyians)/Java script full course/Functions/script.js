25ae58e588845173e37460a63fb34e707fa1c503
Abdul Munam <abdulmunam791@gmail.com>
2025-07-09 03:09:40 +0500

    Update script.js

--- JS revision (shreyians)/Java script full course/Functions/script.js
+++ JS revision (shreyians)/Java script full course/Functions/script.js
@@ -19,7 +19,7 @@ sayBye("Ali");
 function welcome(person) {
   console.log("Welcome, " + person + "!");
 }
-welcome("Ayesha"); // "Ayesha" is an argument, "person" is a parameter
+welcome("broooo"); // "broo" is an argument, "person" is a parameter
 
 // ✅ Default Parameters
 function multiply(a, b = 2) {
@@ -40,4 +40,74 @@ function sumAll(...numbers) {
 }
 console.log("sumAll(1, 2, 3):", sumAll(1, 2, 3));           // 6
 console.log("sumAll(5, 10, 15, 20):", sumAll(5, 10, 15, 20)); // 50
+
+
+// higher order function khelaye ga
+
+//aik yeah tarkika hai 
+function abcd(v){
+
+}
+
+abcd(function(){
+  
+})
+
+// //dosra trakia
+function bcde(){
+  return function(){
+
+  }
+}
+
+
+bcde();
+
+// or agar yeah dono hou bhe tou yeah higher order fnc khelaye ga
+
+
+
+// pure function 
+
+let a = 12;
+function bfe(){
+  console.log("broo")
+}
+bfe();
+
+// impure function
+
+let b = 14;
+function grte(){
+  b++
+}
+grte();
+
+
+// cloures 
+
+function abcde(){
+  let a = 12;
+  return function(){
+    console.log(a);
+  }
+};
+
+// this whole is called is cloures
+
+
+// lexical scoping 
+
+function abcd(){
+  let a = 12;
+
+  function efg(){
+    let b = 34;
+
+    function hijk(){
+      let c = 56;
+    }
+  }
+}
+
 
