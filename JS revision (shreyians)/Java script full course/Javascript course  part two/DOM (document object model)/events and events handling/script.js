f8563cd5afa6dc9942c3c5c7413524b75e9b8821
Abdul Munam <abdulmunam791@gmail.com>
2025-08-01 04:21:57 +0500

    Update script.js

---
+++ JS revision (shreyians)/Java script full course/Javascript course  part two/DOM (document object model)/events and events handling/script.js
@@ -0,0 +1,93 @@
+let sel = document.querySelector("select");
+let heading = document.querySelector("h3");
+heading.style.textTransform = "capitalize"
+sel.addEventListener("change", function(dets){
+   heading.textContent = `${dets.target.value} device selected`
+});
+
+let text = document.querySelector("h1");
+window.addEventListener("keydown", function(dets){
+   if(dets.key === " "){
+      text.textContent = "SPC"
+   }
+   else{
+      text.textContent = dets.key
+   }
+})
+
+
+let btn = document.querySelector(".btn");
+let fileinp = document.querySelector(".fileinp")
+
+btn.addEventListener("click", function(){
+    fileinp.click();
+});
+
+fileinp.addEventListener("change", function(dets){
+   const file = dets.target.files[0]
+   if(file){
+    btn.textContent = file.name
+   }
+});
+
+let form = document.querySelector("form");
+let inputs = document.querySelectorAll("input");
+let main = document.querySelector(".main")
+
+form.addEventListener("submit", function(dets){
+    dets.preventDefault();
+
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let profle = document.createElement("div");
+    profle.classList.add("profile");
+
+    card.appendChild(profle)
+
+    let img = document.createElement("img");
+    img.setAttribute("src", inputs[0].value);
+
+
+
+    let h3 = document.createElement("h3");
+    h3.textContent = inputs[1].value
+    let h5 = document.createElement("h5");
+    h5.textContent = inputs[2].value
+    let p = document.createElement("p");
+    p.textContent = inputs[3].value
+
+    profle.appendChild(img);
+    card.appendChild(profle);
+
+    card.appendChild(h3)
+    card.appendChild(h5)
+    card.appendChild(p)
+
+main.appendChild(card)
+
+inputs.forEach(function(inp){
+    if(inp.type !== "submit"){
+        inp.value = ""
+
+    }
+});
+
+});
+
+
+
+let abcd = document.querySelector(".ABCD");
+
+abcd.addEventListener("mouseover", function(){
+    abcd.style.backgroundColor = "yellow"
+})
+abcd.addEventListener("mouseout", function(){
+    abcd.style.backgroundColor = "red"
+})
+
+
+window.addEventListener("mousemove", function(dets){
+    abcd.style.top = dets.clientY + "px";
+    abcd.style.left = dets.clientX + "px"
+});
