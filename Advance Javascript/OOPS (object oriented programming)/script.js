273d601884b080c27e333d47599168e641a8f6ce
Abdul Munam <abdulmunam791@gmail.com>
2025-08-15 02:10:28 +0500

    Mostly concept of OOPS

--- Advance Javascript/OOPS (object oriented programming)/script.js
+++ Advance Javascript/OOPS (object oriented programming)/script.js
@@ -1,33 +1,38 @@
-//  // This is called a constructor function 
-function Bakebiscuit(name, company, qty, price, flvaour){
-    this.name = name;
-    this.company = company;
-    this.qty = qty;
-    this.price = price;
-    this.flvaoure = flvaour;
-}
-Bakebiscuit.prototype.manfacutrer = "Pakistan"
- let choclati = new Bakebiscuit("Sooper", "peekfrience", 10, 50, "special choclate");
+ function bakebiscuit(name, company, flavour, manfacturer){
+  this.name = name;
+  this.company = company;
+  this.manfacturer = manfacturer;
+  this.flavour = flavour;  
+ };
 
-// Through class
+  let biscuit = new bakebiscuit("sooper", "peelfrience", "choclate", "Pakistan");
 
-class biscuit{
-    constructor(name, company, qty, price, flavour, c){
+  console.log(biscuit);
+
+
+  class Clothes{
+    constructor(name, qty, manfacturer, industryname){
         this.name = name;
-        this.company = company;
-        this.flavour = flavour;
-        this.price = price;
+        this.qty = qty;
+        this.manfacturer = manfacturer
+        this.industryname = industryname
+    }
+  }
+  let newclothes = new Clothes("cotton", "pure", "Pakistan", "getstextile")
+  let oldclothes = new Clothes("cotton", "used", "Pakistan","getstextile")
+
+  console.log(newclothes);
+  console.log(oldclothes);
+
+
+  class Admin extends Clothes{
+    constructor(CEO){
+    super("cotton", "pure", "Pakistan", "getstextile")
+    this.CEO = CEO;
+
     }
 
-    write(text){
-        let h1 = document.createElement("h1");
-        h1.textContent = text;
-        h1.style.color  = this.color
-        document.body.appendChild(h1);
-    }
-}
+  }
 
-let bake = new biscuit("Sooper", 10, "peelfrience", "balaabl", "yellow")
-
- 
-
+  let CEO = new Admin("ELon Musk");
+  console.log(CEO);
