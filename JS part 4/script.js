0eb6a8177f13b397ec50339fa2c3bcad2f7eb1b7
Abdul Munam <abdulmunam791@gmail.com>
2025-10-18 16:06:00 +0500

    Add files via upload

---
+++ JS part 4/script.js
@@ -0,0 +1,59 @@
+// module pattern
+// let Bank = (function() {
+//     let bankbalance = 12000;
+
+//     function checkblance(){
+//         console.log(bankbalance)
+//     };
+
+//         function setbalance(val){
+//             bankbalance = val
+//         }
+//     function withdraw(val){
+//         if(val <= bankbalance){
+//             val -= bankbalance;
+//             console.log(bankbalance);
+//         };
+//     }
+
+//     return{
+//         checkblance,
+//         setbalance,
+//         withdraw,
+//     }
+// })();
+
+// Factory function pattern
+
+//     function creatproduct(name, price) {
+//         let stock = 10;
+
+//         return {
+//             name,
+//             price,
+
+//             checkstock(){
+//                 console.log(stock);
+//             },
+//             buy(qty){
+//                 if(qty <= stock){
+//                     stock -= qty;
+//                     console.log(`${qty} is booked and  ${stock} pieces left`)
+//                 }
+//                 else{
+//                     console.error(`there is just ${stock} pieces`)
+//                 };
+
+//             },
+//             refill(qty){
+//                 stock += qty;
+//                 console.log(`${qty} pecies are placed`);
+//             }
+//         }
+//     }
+
+
+//  let hp = creatproduct("laptop", 70000);
+//  let apple= creatproduct("iphone", 200000)
+
+
